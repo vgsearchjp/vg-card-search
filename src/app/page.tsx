92be166eabba964189d2831c45e6e4d2585cd8ef
vgsearchjp <290398754+vgsearchjp@users.noreply.github.com>
2026-07-12 22:52:15 +0900

    変更内容

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2047,7 +2047,7 @@ const saveDeckImage = async () => {
 
   setSavingDeckImage(true);
 
-  await new Promise((resolve) => setTimeout(resolve, 50));
+  await new Promise((resolve) => setTimeout(resolve, 50000));
 
 let dataUrl = "";
 
