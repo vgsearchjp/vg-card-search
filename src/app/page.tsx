c4512910e1b27a6512f08ed745146767fe249d56
vgsearchjp <290398754+vgsearchjp@users.noreply.github.com>
2026-09-15 23:21:42 +0900

    変更内容

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5067,7 +5067,7 @@ className="
   </h2>
 </div>
 
-<div className="w-full max-w-[1000px] mx-auto aspect-[16/10] border-2 border-gray-400 rounded-lg bg-gray-100 relative max-md:landscape:fixed max-md:landscape:inset-y-0 max-md:landscape:-left-[5vw] max-md:landscape:z-[9999] max-md:landscape:w-[110vw] max-md:landscape:h-[100dvh] max-md:landscape:max-w-none max-md:landscape:aspect-auto max-md:landscape:mx-0 max-md:landscape:border-0 max-md:landscape:rounded-none">
+<div className="w-full max-w-[1000px] mx-auto aspect-[16/10] border-2 border-gray-400 rounded-lg bg-gray-100 relative max-md:landscape:fixed max-md:landscape:inset-0 max-md:landscape:z-[9999] max-md:landscape:w-screen max-md:landscape:h-[100dvh] max-md:landscape:max-w-none max-md:landscape:aspect-auto max-md:landscape:mx-0 max-md:landscape:border-0 max-md:landscape:rounded-none">
 
 {/* オーダー */}
 <div
