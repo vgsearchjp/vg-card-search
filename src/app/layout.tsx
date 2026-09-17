f0dafadd45e7bc861c4fd60bb1d8791fe2d5bc30
vgsearchjp <290398754+vgsearchjp@users.noreply.github.com>
2026-09-17 22:20:01 +0900

    変更内容

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+<html
+  lang="en"
+  className={`${geistSans.variable} ${geistMono.variable} h-full w-full antialiased`}
+>
+  <body className="min-h-full flex flex-col">{children}</body>
+</html>
   );
 }
