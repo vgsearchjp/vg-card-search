0b5f0f0f19093de37f9ef63410ad77b7239f14ca
vgsearchjp <290398754+vgsearchjp@users.noreply.github.com>
2026-09-17 20:06:16 +0900

    変更内容

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5067,9 +5067,9 @@ className="
   </h2>
 </div>
 
-<div className="relative w-full h-full max-w-none mx-auto aspect-[16/10] max-md:landscape:fixed max-md:landscape:inset-0 max-md:landscape:z-[9999] max-md:landscape:w-screen max-md:landscape:h-[100dvh] max-md:landscape:max-w-none max-md:landscape:aspect-auto max-md:landscape:mx-0 max-md:landscape:border-0 max-md:landscape:rounded-none">
+<div className="relative w-full h-full max-w-none mx-auto aspect-[16/10] max-md:landscape:fixed max-md:landscape:inset-0 max-md:landscape:z-[9999] max-md:landscape:w-[130vw] max-md:landscape:h-[100dvh] max-md:landscape:max-w-none max-md:landscape:aspect-auto max-md:landscape:mx-0 max-md:landscape:border-0 max-md:landscape:rounded-none">
 
-<div className="absolute top-0 left-0 w-full h-full bg-gray-100 border-2 border-gray-400 rounded-lg pointer-events-none max-md:landscape:left-[-25vw] max-md:landscape:w-[150vw]" />
+<div className="absolute top-0 left-[-25vw] w-[150vw] h-full bg-gray-100 border-2 border-gray-400 rounded-lg pointer-events-none" />
 
 {/* オーダー */}
 <div
