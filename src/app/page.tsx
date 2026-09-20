96ae5fad887cea396591224c54d43df75092ea1c
vgsearchjp <290398754+vgsearchjp@users.noreply.github.com>
2026-09-20 22:34:44 +0900

    変更内容

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5212,7 +5212,7 @@ if (orderCard.length > 0) {
       setSelectedMoveSource(null);
     }}
     disabled={!!triggerCard || onePlayerDeck.length === 0}
-   className="w-[65px] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:w-[105px] translate-x-2 [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:translate-x-0 text-sm bg-blue-500 text-white px-2 py-1 rounded disabled:bg-gray-400 whitespace-nowrap"
+   className="w-[70px] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:w-[105px] translate-x-2 [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:translate-x-0 text-sm bg-blue-500 text-white px-2 py-1 rounded disabled:bg-gray-400 whitespace-nowrap"
   >
     トリガー 
   </button>
@@ -5267,7 +5267,7 @@ if (orderCard.length > 0) {
     {/* リセット */}
     <button
       onClick={resetOnePlayerBoard}
-     className="w-[28px] h-[70px] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:w-[40px] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:h-[105px] -translate-y-3 [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:translate-y-0 bg-gray-500 text-white rounded text-sm flex items-center justify-center"
+     className="w-[28px] h-[70px] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:w-[40px] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:h-[105px] -translate-y-4 [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:translate-y-0 bg-gray-500 text-white rounded text-sm flex items-center justify-center"
     >
       <span className="[writing-mode:vertical-rl]">リセット</span>
     </button>
