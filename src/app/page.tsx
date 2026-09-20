53b8e13741b05d054eeff6104a9a39cfece27f43
vgsearchjp <290398754+vgsearchjp@users.noreply.github.com>
2026-09-20 23:44:02 +0900

    変更内容

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7254,7 +7254,7 @@ if (backRightRCard) {
 
 {/* ドロップ */}
 <div
-  className="absolute top-[70%] md:top-[75%] right-[5%] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:right-[1%] z-20 flex items-center gap-3"
+  className="absolute top-[69%] md:top-[75%] right-[5.5%] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:right-[1%] z-20 flex items-center gap-3"
 >
 <div
 onClick={() => {
@@ -7311,9 +7311,9 @@ if (selectedMoveSource === "trigger") {
       )}
     </div>
 
-    <span className="w-[28px] h-[95px] text-sm md:text-lg [writing-mode:vertical-rl] bg-blue-500 text-white rounded flex items-center justify-center">
-      ドロップ
-    </span>
+<span className="w-[28px] h-[95px] text-xs [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:w-[32px] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:h-[110px] [@media(min-width:768px)_and_(hover:hover)_and_(pointer:fine)]:text-lg [writing-mode:vertical-rl] bg-blue-500 text-white rounded flex items-center justify-center">
+  ドロップ
+</span>
   </div>
 </div>
 
